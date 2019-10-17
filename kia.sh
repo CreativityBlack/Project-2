89ea7c517f2b0f55294e3f22f016117d0fa480bb
kiadorii <sgchoi529@gmail.com>
2019-10-17 15:53:44 -0700

    Working Changes

--- kia.sh
+++ kia.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+# Which Product
+# Standard || Transforamtion || Engineering Change
+# Preload || Activation
+
+# WHen will this start? - 30, 20, 10, now
+# What is the Target bundle
+
+echo "❤ Hello! Please choose a Menu ❤"
+
+read mainCommand
+
