c45b532ab4b8ae462ec5ff7631732735c038d5bb
krishanj <ksyles@gmail.com>
2019-10-18 14:13:50 -0700

    final

--- test.sh
+++ test.sh
@@ -19,21 +19,25 @@ read pmrNumber
 echo "Preload or Activation"
 read preloadActivation
 
+#Create a while loop to add the ics extra strings into icsImages and loop through until user type done
 set icsImages = " "
 icsArray=()
 while [ "$icsImages" != "done" ]
-do
-echo "Other ICS Images"
-read icsImages
-echo "$icsImages"
-icsArray+=( "$icsImages" )
+    do
+    #input and read the extra images
+    echo "•	Any other ICS image(s) that will need to be re-installed after the code is updated"
+    read icsImages
+    #echo "$icsImages"
+    #checks to see if the user hasnt inputed done
+    if [ "$icsImages" != "done" ]
+    then
+    #adds the string to the array
+        icsArray+=( "$icsImages" )
+    fi
+    #echo ${icsArray[*]}
+ done
+#echo "$icsImages"
 echo ${icsArray[*]}
-#set -A icsArray ${array[*]} $icsImages 
-
-
-
-done 
-echo "$icsImages"
 
 #product type of DS8K
 if [ "$productName" == "DS8K" ]
@@ -43,44 +47,53 @@ echo "DS8K"
     if [ "$loadName" == "standard" ]
     then
     echo "standard"
-        #preload type
+        #standard preload type
         if [ "$preloadActivation" == "preload" ] 
         then
         echo "preload"
             #finds the place in the text and creates a newletter with the text
             #find_replace () {
             sed -n '336,354p' Templates.txt > newletter.txt
-            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g" newletter.txt 
-          #at newletter.txt
-           # }
-        #find_replace
-        fi
-        #activation type
-        if [ "$preloadActication" == "activation" ] 
+
+            #Replaces the specif string for type/model/serial number and adds extra ics names
+            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g; s/•	Any other ICS image(s) that will need to be re-installed after the code is updated/${icsArray[*]}/g" newletter.txt 
+        #Standard activation
+        elif [ "$preloadActivation" == "activation" ] 
         then
         echo "iam in activation"
             #finds the place in the text and creates a newletter with the text
             
             sed -n '336,354p' Templates.txt > newletter.txt
-            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g" newletter.txt            }
+
+
+            #Replaces the specif string for type/model/serial number and ics names
+            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g; s/•	Any other ICS image(s) that will need to be re-installed after the code is updated/${icsArray[*]}/g" newletter.txt 
         fi
-    fi
+    
     #transformation type of load
-    if [ "$loadName" == "transformation" ]
+    elif [ "$loadName" == "transformation" ]
     then
-        #preload load
+    echo "i am in transformation"
+        #transformation preload load
         if [ "$preloadActivation" == "preload" ] 
         then
+        echo "hello in transf preload"
             #finds the place in the text and creates a newletter with the text
             sed -n '336,354p' Templates.txt > newletter.txt
-            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g" newletter.txt
-        fi
-        #Activation load
-        if [ "$preloadActication" == "activation" ] 
+
+
+            #Replaces the specif string for type/model/serial number and ics names
+            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g; s/•	Any other ICS image(s) that will need to be re-installed after the code is updated/${icsArray[*]}/g" newletter.txt 
+        
+        #transfromation Activation load
+        elif [ "$preloadActivation" == "activation" ] 
         then
+        echo "hello in trans acti"
             #finds the place in the text and creates a newletter with the text
             sed -n '336,354p' Templates.txt > newletter.txt
-            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g" newletter.txt
+
+            #Replaces the specif string for type/model/serial number and ics names
+            sed "s/(2834-985-75#####)/$typemodserNumber/g; s/IBM Ticket:/IBM Ticket: $ticketNumber/g; s/PMR:/PMR: $pmrNumber/g; s/•	Any other ICS image(s) that will need to be re-installed after the code is updated/${icsArray[*]}/g" newletter.txt 
         fi
     fi
 
